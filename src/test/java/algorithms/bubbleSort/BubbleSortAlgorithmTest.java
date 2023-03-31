3f43c9dc0e6c5b97d9174db995273d0717d8dd35
Stidz-PC <marszp8888@gmail.com>
2023-04-01 00:01:06 +0200

    Add tests for BubbleSort

--- src/test/java/algorithms/bubbleSort/BubbleSortAlgorithmTest.java
+++ src/test/java/algorithms/bubbleSort/BubbleSortAlgorithmTest.java
@@ -1,4 +1,28 @@
+package algorithms.bubbleSort;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class BubbleSortAlgorithmTest {
-  
+
+    int[] arrayToSort = {-1, 4, -3, 2, 51};
+    int[] resultAscending = {-3, -1, 2, 4, 51};
+    int[] resultDescending = {51, 4, 2, -1, -3};
+    BubbleSortAlgorithm tempBubbleSortObject = new BubbleSortAlgorithm();
+
+    @Test
+    public void bubleSortNullObject() {
+        assertFalse(tempBubbleSortObject.equals(null));
+    }
+
+    @Test
+    public void bubbleSortMethodAscending() {
+        assertArrayEquals(resultAscending, tempBubbleSortObject.bubbleSortMethodAscending(arrayToSort));
+    }
+
+    @Test
+    public void bubbleSortMethodDescending() {
+        assertArrayEquals(resultDescending, tempBubbleSortObject.bubbleSortMethodDescending(arrayToSort));
+    }
 }
