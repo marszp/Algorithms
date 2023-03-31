09c00435fd8cd922f27cb935d1066c62cca9f746
Stidz-PC <marszp8888@gmail.com>
2023-03-31 23:57:39 +0200

    Untrack unnecessary files

--- src/main/java/oldMethodsNotIncludedOnGit/Sortable.java
+++
@@ -1,6 +0,0 @@
-package algorithms.bubbleSort;
-
-public interface Sortable {
-    <T extends Comparable<T>> void sortAscending(T[] values);
-    <T extends Comparable<T>> void sortDescending(T[] values);
-}
