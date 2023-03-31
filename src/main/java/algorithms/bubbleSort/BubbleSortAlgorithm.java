cb043409fe64cce8c00f15ec1d54566e07671ea8
Stidz-PC <marszp8888@gmail.com>
2023-03-31 23:50:44 +0200

    Simple implementation of Bubble Sort Algorithm + Unit Tests

---
+++ src/main/java/algorithms/bubbleSort/BubbleSortAlgorithm.java
@@ -0,0 +1,11 @@
+package algorithms.bubbleSort;
+
+public class BuubleSortAlgorithm {
+
+
+    public static void swap(int[] arrayToHandle, int first, int second){
+        int tempValue = second;
+        second = first;
+        first = tempValue;
+    }
+}
